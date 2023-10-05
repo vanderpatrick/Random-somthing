9c5635dd57fcf61eaf57e1fbb9c45d6ff0060252
= <=>
2023-10-05 21:28:47 +0200

    vtnc esse krl allowence ta me fudendo mas vdb

--- money_validation.py
+++ money_validation.py
@@ -1,3 +1,5 @@
+from decimal import *
+
 # make a hashmap of fixed bills that we need to pay.
 fixed_bills_dict = {
     "Rent": 877.5,
@@ -7,18 +9,46 @@ fixed_bills_dict = {
     "Current internet plan": 32.76,
     "Spotify": 11,
 }
-# create var for current salary
-current_salary = float(2114.13)
+# Loose variables
+current_salary: Decimal = Decimal(2114.13)
+sum_of_bills: Decimal = Decimal(sum(fixed_bills_dict.values()))
+current_savings_percentage: Decimal = Decimal(0.60)
 
 
-# process fixed bills in relation to current salary to get liquid cash
-def liquid_cash(current_money: float) -> float:
-    sum_of_bills = sum(fixed_bills_dict.values())
-    money_after_bills = int(current_money - sum_of_bills)
-
-    return money_after_bills
+# This function validates input to decimals
+def input_validation(user_input: Decimal) -> Decimal:
+    return Decimal(user_input).quantize(Decimal("0.01"), rounding=ROUND_DOWN)
 
 
-current_processed_money = liquid_cash(current_money=current_salary)
+# Returns liquid amount of cash
+def liquid_cash_per_month(my_salary: Decimal, my_total_bills: Decimal) -> Decimal:
+    return my_salary - my_total_bills
 
-print(current_processed_money)
+
+# saving function i want 60% to go to savings
+def saving(validated_liquid_cash: Decimal, percentage: Decimal) -> Decimal:
+    return percentage * validated_liquid_cash
+
+
+# allowance function
+def allowance() -> Decimal:
+    pass
+
+
+def main():
+    my_salary = input_validation(current_salary)
+    my_total_bills = input_validation(sum_of_bills)
+    month_liquid_money = liquid_cash_per_month(my_salary, my_total_bills)
+    amount_to_savings = input_validation(
+        saving(month_liquid_money, current_savings_percentage)
+    )
+    test = allowance()
+    print(f"{my_salary = }")
+    print(f"{my_total_bills = }")
+    print(f"{month_liquid_money = }")
+    print(f"{amount_to_savings =}")
+    print(test)
+
+
+if __name__ == "__main__":
+    main()
